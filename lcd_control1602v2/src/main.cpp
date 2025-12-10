9631bc79cdd02aba5a5d923e1a2acb63dbdef3ef
Cyrille G G <c.guimapi@yahoo.fr>
2025-12-10 15:31:15 +0100

    lcd didplay any string

---
+++ lcd_control1602v2/src/main.cpp
@@ -0,0 +1,12 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
+#include "lcd.h" 
+
+void setup() {  
+  LCD_init();
+}  
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
